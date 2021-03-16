9743b7bd511f2f567921902a9cb8d543aed276b3
taylor-e-s <202455@pennmanor.net>
2021-03-16 09:03:13 -0400

    update wifi connectivity

--- CSWifi.py
+++ CSWifi.py
@@ -1,6 +1,16 @@
+import datetime
 
 def get_time():
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     print("Current Time =", current_time)
+
+#wifi connectivity
+import urllib.request
+def connect(host='http://google.com'):
+    try:
+        urllib.request.urlopen(host)
+        return True
+    except:
+        return False
 
