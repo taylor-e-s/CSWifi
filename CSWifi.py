32894792f31993d216d1c014673617d7b6243706
cdevinney <cedevinney@gmail.com>
2021-03-16 08:36:23 -0400

    Defining get_time()
    Defining disconnection_warning()

--- CSWifi.py
+++ CSWifi.py
@@ -1,17 +1,8 @@
-#Taylor Shirk  Claire DeVinney
-import tkinter
-parent = tkinter.Tk()
-parent.overrideredirect(1)
-parent.iconbitmap("PythonIcon.ico")
-parent.withdraw()
-from tkinter import messagebox
+
 
 def get_time():
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     print("Current Time =", current_time)
-
-def disconnection_warning():
-    yesno = messagebox.askyesno('Your internet connection has been lost.', 'Would you like to reconnect it?', parent=parent)
 
 #attempting to connecting to wifi
