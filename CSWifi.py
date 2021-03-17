3bf67572a2ff26df397c4137bfc9fad3ac96b611
cdevinney <cedevinney@gmail.com>
2021-03-17 08:58:37 -0400

    clear out incorrect code

--- CSWifi.py
+++ CSWifi.py
@@ -1,17 +1,19 @@
 import datetime
-disconnections = []
+import os
 
 def get_time():
-    now = datetime.time()
+    now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     print("Current Time =", current_time)
+
+os.system('zenity --question --text="Does this work?"')
 
 #wifi connectivity
 import urllib.request
 def connect(host='http://google.com'):
     try:
         urllib.request.urlopen(host)
-        print("True")
+        return True
     except:
-        disconnections.append(current_time)
+        return False
 
