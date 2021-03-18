7686923e1536914bc8be4566396d5ef71573e9fb
cdevinney <cedevinney@gmail.com>
2021-03-18 08:14:14 -0400

    rearranging code

--- CSWifi.py
+++ CSWifi.py
@@ -1,21 +1,28 @@
 import datetime
 import os
+import urllib.request
 from urllib3.util import current_time
 disconnections = []
+
 
 def get_time():
     now = datetime.time()
     current_time = now.strftime("%H:%M:%S")
 
-os.system('zenity --question --text="Does this work?"')
-os.system("shutdown -r")
+def ask_question():
+    if os.system('zenity --question --text="Your internet connection has been lost. Would you like to reconnect?"'): #if 'No' is clicked
+        os.system('zenity --info --text="You have chosen not to reconnect."')
+    else: #if 'Yes' is clicked
+        os.system('zenity --info --text="That function is not ready yet."') #
+
+#os.system("shutdown -r")
 
 #wifi connectivity
-import urllib.request
 def connect(host='http://google.com'):
     try:
         urllib.request.urlopen(host)
         return True
     except:
         disconnections.append(current_time)
+        ask_question()
 
