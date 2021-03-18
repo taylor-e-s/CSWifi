109ac9b53d2b2a5825044b7fecc57825c2d8f94d
cdevinney <cedevinney@gmail.com>
2021-03-18 08:34:29 -0400

    adding ask_restart

--- CSWifi.py
+++ CSWifi.py
@@ -10,13 +10,19 @@ def add_time():
     current_time = now.strftime("%H:%M:%S")
     disconnections.append(current_time)
 
-def ask_question():
+def ask_reconnection():
     if os.system('zenity --question --text="Your internet connection has been lost. Would you like to reconnect?"'): #if 'No' is clicked
         os.system('zenity --info --text="You have chosen not to reconnect."')
     else: #if 'Yes' is clicked
         os.system('zenity --info --text="Reconnecting..."')
         return True
 
+def ask_restart():
+    if os.system('zenity --question --text="Reconnection failed. Would you like to restart?"'): #if 'No' is clicked
+        os.system('zenity --info --text="You have chosen not to restart."')
+    else: #if 'Yes' is clicked
+        os.system('zenity --info --text="Restarting..."')
+        return True
 
 #wifi connectivity
 def detect(host='http://google.com'):
