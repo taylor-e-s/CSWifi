96fdbb5c750680c1884ae8ca209bddc3debcc359
cdevinney <cedevinney@gmail.com>
2021-03-18 08:20:02 -0400

    renaming function

--- CSWifi.py
+++ CSWifi.py
@@ -2,7 +2,6 @@ import datetime
 import os
 import urllib.request
 from urllib3.util import current_time
-
 disconnections = []
 
 
@@ -11,22 +10,21 @@ def add_time():
     current_time = now.strftime("%H:%M:%S")
     disconnections.append(current_time)
 
-
 def ask_question():
-    if os.system(
-            'zenity --question --text="Your internet connection has been lost. Would you like to reconnect?"'):  # if 'No' is clicked
+    if os.system('zenity --question --text="Your internet connection has been lost. Would you like to reconnect?"'): #if 'No' is clicked
         os.system('zenity --info --text="You have chosen not to reconnect."')
-    else:  # if 'Yes' is clicked
-        os.system('zenity --info --text="That function is not ready yet."')  #
+    else: #if 'Yes' is clicked
+        os.system('zenity --info --text="Reconnecting..."')
+        return True
 
+#os.system("shutdown -r")
 
-#os.system("shutdown -r -t 0")
-
-# wifi connectivity
-def connect(host='http://google.com'):
+#wifi connectivity
+def detect(host='http://google.com'):
     try:
         urllib.request.urlopen(host)
         return True
     except:
         add_time()
-        ask_question()
+        if ask_question() = True:
+
