341e4877d2fbd69b19a3c04466f8eaaa5f4155c6
taylor-e-s <202455@pennmanor.net>
2021-03-18 08:15:50 -0400

    fix shutdown code

--- CSWifi.py
+++ CSWifi.py
@@ -2,6 +2,7 @@ import datetime
 import os
 import urllib.request
 from urllib3.util import current_time
+
 disconnections = []
 
 
@@ -9,15 +10,18 @@ def get_time():
     now = datetime.time()
     current_time = now.strftime("%H:%M:%S")
 
+
 def ask_question():
-    if os.system('zenity --question --text="Your internet connection has been lost. Would you like to reconnect?"'): #if 'No' is clicked
+    if os.system(
+            'zenity --question --text="Your internet connection has been lost. Would you like to reconnect?"'):  # if 'No' is clicked
         os.system('zenity --info --text="You have chosen not to reconnect."')
-    else: #if 'Yes' is clicked
-        os.system('zenity --info --text="That function is not ready yet."') #
+    else:  # if 'Yes' is clicked
+        os.system('zenity --info --text="That function is not ready yet."')  #
 
-#os.system("shutdown -r")
 
-#wifi connectivity
+#os.system("shutdown -r -t 0")
+
+# wifi connectivity
 def connect(host='http://google.com'):
     try:
         urllib.request.urlopen(host)
@@ -25,4 +29,3 @@ def connect(host='http://google.com'):
     except:
         disconnections.append(current_time)
         ask_question()
-
