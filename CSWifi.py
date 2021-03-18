57e4037fbfd2ec908c5bcb36f93b6a502ffdd29d
taylor-e-s <202455@pennmanor.net>
2021-03-18 07:53:06 -0400

    removing print

--- CSWifi.py
+++ CSWifi.py
@@ -1,13 +1,11 @@
 import datetime
 import os
 from urllib3.util import current_time
-
 disconnections = []
 
 def get_time():
     now = datetime.time()
     current_time = now.strftime("%H:%M:%S")
-    print("Current Time =", current_time)
 
 os.system('zenity --question --text="Does this work?"')
 os.system("shutdown /r")
@@ -17,7 +15,7 @@ import urllib.request
 def connect(host='http://google.com'):
     try:
         urllib.request.urlopen(host)
-        print("True")
+        return True
     except:
         disconnections.append(current_time)
 
