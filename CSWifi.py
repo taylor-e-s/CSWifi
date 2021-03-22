64327a32024f7c3c274467e13b6ac28ac4c03c74
taylor-e-s <202455@pennmanor.net>
2021-03-22 08:04:48 -0400

    adding comments

--- CSWifi.py
+++ CSWifi.py
@@ -23,6 +23,7 @@ def main():
 
 
 def add_time():
+#finds the current time, adds it to the list, and displays the list
     now = datetime.time()
     current_time = now.strftime("%H:%M:%S")
     disconnections.append(current_time)
@@ -46,6 +47,7 @@ def ask_restart():
 
 
 def detect(host='http://google.com'):
+    #checks wifi connectivity by trying to connect to google
     try:
         urllib.request.urlopen(host)
         return True
@@ -55,6 +57,7 @@ def detect(host='http://google.com'):
 
 
 def reconnect():
+    #disconnects and reconnects to the wifi
     os.system('netsh interface set interface "Wi-Fi" disable')
     os.system('netsh interface set interface "Wi-Fi" enable')
     os.system('netsh wlan connect name="student"')
